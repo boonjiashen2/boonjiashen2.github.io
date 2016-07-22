183104762a6fc75f8d4fdbb43db4ab5083299b8d
jiashen <jiashen@jiashens-MacBook-Air-2.local>
2016-07-21 22:38:45 -0700

    Add more timer stuff

--- js/tooltipFirstTime.js
+++ js/tooltipFirstTime.js
@@ -1,11 +1,66 @@
-tooltip = $("#content");
+CONTENT = $("#content");
+DISCOVERY_COOKIE = 'isDiscovered';
+TIMER_COOKIE = 'timer';
+MAX_ELAPSED_TIME_IN_SECONDS = 3;
 
-function hideTooltip() {
-    tooltip.css('display', 'none');
+function onload() {
+    if (getDiscoveredBit()) {
+        hideTooltip();
+    }
+    else if (hasTimerStarted() && getElapsedTimeInSeconds() > MAX_ELAPSED_TIME_IN_SECONDS) {
+        hideTooltip();
+    }
+    else {
+        startTimer();
+    }
 }
 
-$("#content").append('boogers');
-$("#content").click(hideTooltip);
-Cookies.set('test', 5);
-console.log(Cookies.get('test'));
-console.log(11);
+function hideTooltip() {
+    CONTENT.css('display', 'none');
+}
+
+function hideTooltipForever() {
+    hideTooltip();
+    setDiscoveredBit();
+}
+
+function getEpochTimeNowInMilliseconds() {
+    return (new Date).getTime();
+}
+
+function startTimer() {
+    Cookies.set(TIMER_COOKIE, getEpochTimeNowInMilliseconds());
+}
+
+function getElapsedTimeInSeconds() {
+    if (!hasTimerStarted()) {
+        return -1;
+    }
+    else {
+        var elapsedTimeInMilliseconds = (getEpochTimeNowInMilliseconds() - Cookies.get(TIMER_COOKIE));
+        return elapsedTimeInMilliseconds / 1000;
+    }
+}
+
+function hasTimerStarted() {
+    return Cookies.get(TIMER_COOKIE) != null;
+}
+
+function setDiscoveredBit() {
+    Cookies.set(DISCOVERY_COOKIE, null);
+}
+
+function getDiscoveredBit() {
+    return Cookies.get(DISCOVERY_COOKIE) != null;
+}
+
+$("#content").click(hideTooltipForever);
+
+onload();
+setDiscoveredBit();
+getDiscoveredBit();
+getEpochTimeNowInMilliseconds();
+getElapsedTimeInSeconds();
+startTimer();
+hasTimerStarted();
+console.log('End of js');
